633cb9caa96417fe548aefb1d8d023f732de604c
Account24033 <garciay@wustl.edu>
2025-09-10 13:37:19 -0500

    progress

---
+++ src/studio2/Ruin.java
@@ -0,0 +1,31 @@
+///import java.util.Scanner;
+
+public class Ruin {
+public static void main (String[]args){
+    /*Scanner in = new Scanner(System.in);
+    System.out.println("What do you want the start amount to be?");
+    int startAmount = in.nextInt();
+    System.out.println(x:"Win chance?:");
+    double.winChange = in.nextDouble();
+    System.out.println(x:"winLimit?:");
+    int winLimit = in.nextInt();
+    */
+    int startAmount = 2;
+    double winChance = .7;
+    int winLimit = 10;
+    int totalSimulations = 20;
+
+
+    for(int sim = 1; sim<totalSimulations; sim++);{
+        for(int money = startAmount; money > 0;); {
+
+        }
+
+    }
+
+
+
+    
+}
+
+}
