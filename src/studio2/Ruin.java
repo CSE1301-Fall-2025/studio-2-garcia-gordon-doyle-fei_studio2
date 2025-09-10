9b404b4fa166c4c732088125df66e632f47905e1
Nate Gordon <natemax07@gmail.com>
2025-09-10 14:12:58 -0500

    last commit

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -1,23 +1,38 @@
-///import java.util.Scanner;
+import java.util.Scanner;
+
+import java.lang.Math;
 
 public class Ruin {
 public static void main (String[]args){
-    /*Scanner in = new Scanner(System.in);
+    Scanner in = new Scanner(System.in);
     System.out.println("What do you want the start amount to be?");
     int startAmount = in.nextInt();
-    System.out.println(x:"Win chance?:");
-    double.winChange = in.nextDouble();
-    System.out.println(x:"winLimit?:");
+    System.out.println("Win chance?:");
+    double winChance = in.nextDouble();
+    System.out.println("winLimit?:");
     int winLimit = in.nextInt();
-    */
-    int startAmount = 2;
-    double winChance = .7;
-    int winLimit = 10;
-    int totalSimulations = 20;
+    System.out.println("What is the number of simulations?");
+    int totalSimulations = in.nextInt();
+    in.close();
 
 
-    for(int sim = 1; sim<totalSimulations; sim++);{
-        for(int money = startAmount; money > 0;); {
+    for(int sim = 1; sim<totalSimulations; sim++){
+        double wins = 0;
+        for(int money = startAmount; money > 0;) {
+            if (Math.random() >= winChance) {
+                wins++;
+                money++;
+                if (money >= winLimit) {
+                    System.out.println("You win!"+  " Ruin Rate: " + (sim - wins) / sim);
+                    break;
+                }
+            } else {
+                money--;
+                if (money <= 0) {
+                    System.out.println("You lose!" +  " Ruin Rate: " + (sim - wins) / sim);
+                    break;
+                }
+            }
 
         }
 
